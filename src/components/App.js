05df21c21925606a7ea2ad8f7f18a052d1d7b107
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2024-02-26 18:59:37 +0000

    router commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
 import React from "react";
+import About from "../pages/About.js";
+import Home from '../pages/Home.js'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import './../styles/App.css';
+
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <nav>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/about">about</Link></li>
+          </ul>
+        </nav>
+        
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+
+        </Routes>   
+
     </div>
   )
 }
